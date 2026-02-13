3969f1082f0b4bb26d7605fded7ff60461326d77
Brynley McDonald <Brynley.McDonald@2degrees.nz>
2026-02-13 17:08:35 +1300

    Fix env var mocks

--- ci_build_process_test.go
+++ ci_build_process_test.go
@@ -60,11 +60,16 @@ func testCIBuildProcess(t *testing.T, context spec.G, it spec.S) {
 		summer = &fakes.Summer{}
 
 		environment = &fakes.EnvironmentConfig{}
-		environment.LookupCall.Returns.Value = "some-val"
-		environment.LookupCall.Returns.Found = true
+		environment.LookupCall.Stub = func(key string) (string, bool) {
+			switch key {
+			case "NPM_CONFIG_LOGLEVEL":
+				return "some-val", true
+			default:
+				return "", false
+			}
+		}
 
 		buffer = bytes.NewBuffer(nil)
-		clock = chronos.DefaultClock
 
 		process = npminstall.NewCIBuildProcess(executable, summer, environment, scribe.NewLogger(buffer))
 	})
